2d9baa0db27e6e7d1b9a2e02db4fe19dac5d2f33
Nazoest <122823405+Nazoest@users.noreply.github.com>
2023-06-28 00:26:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App(props) {
+  const age= 18
+  return (
+    <div className="App">
+      <h1>Hello {age}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
